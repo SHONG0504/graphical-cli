ffaeddb66fbfb978fd35990a32c07c8a9ef04a0b
Seungmin Hong <seungminhong2002@gmail.com>
2022-11-27 21:21:47 -0800

    Created display template.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import tkinter
+import os, sys
+
+from tkinter import *
+
+class Display(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Graphical CLI")
+
+        self.current_dir = Label(self, text=home)
+        self.current_dir.place(relx=0, rely=0, anchor=NW)
+
+        self.mainloop()
 
 if __name__ == "__main__":
-    pass
+    home = os.path.expanduser('~')
+
+    display = Display()
